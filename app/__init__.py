6bdba634e50e928467a294bf7521baf4dad97c57
Melyssa Novaes <mellyssasantos889@gmail.com>
2025-11-15 15:31:56 -0300

    adicionar gerenciamento de ciclo de vida ao FastAPI para inicializar o banco de dados

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from app.users.router import user_router
+from app.engine import init_db
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield 
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(user_router)
